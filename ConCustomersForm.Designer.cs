461450bc276afccc251e4158c0a27bf7d8a14405
Nick Cleary <72735342+nickstack-byte@users.noreply.github.com>
2021-02-14 12:09:19 +0000

    Add project files.

---
+++ ConCustomersForm.Designer.cs
@@ -0,0 +1,382 @@
+﻿
+namespace NickSoftEngA2
+{
+    partial class ConCustomers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.textBoxBizName = new System.Windows.Forms.TextBox();
+            this.textBoxAdd1 = new System.Windows.Forms.TextBox();
+            this.textBoxAdd2 = new System.Windows.Forms.TextBox();
+            this.textBoxPoCo = new System.Windows.Forms.TextBox();
+            this.textBoxPhone = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxJobID = new System.Windows.Forms.TextBox();
+            this.textBoxMCost = new System.Windows.Forms.TextBox();
+            this.textBoxJCost = new System.Windows.Forms.TextBox();
+            this.textBoxNotes = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button1st = new System.Windows.Forms.Button();
+            this.buttonPrev = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonLast = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(223, 8);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(27, 26);
+            this.textBoxID.TabIndex = 0;
+            // 
+            // textBoxBizName
+            // 
+            this.textBoxBizName.Location = new System.Drawing.Point(223, 45);
+            this.textBoxBizName.Name = "textBoxBizName";
+            this.textBoxBizName.Size = new System.Drawing.Size(169, 26);
+            this.textBoxBizName.TabIndex = 2;
+            // 
+            // textBoxAdd1
+            // 
+            this.textBoxAdd1.Location = new System.Drawing.Point(223, 87);
+            this.textBoxAdd1.Name = "textBoxAdd1";
+            this.textBoxAdd1.Size = new System.Drawing.Size(169, 26);
+            this.textBoxAdd1.TabIndex = 4;
+            // 
+            // textBoxAdd2
+            // 
+            this.textBoxAdd2.Location = new System.Drawing.Point(223, 128);
+            this.textBoxAdd2.Name = "textBoxAdd2";
+            this.textBoxAdd2.Size = new System.Drawing.Size(169, 26);
+            this.textBoxAdd2.TabIndex = 6;
+            // 
+            // textBoxPoCo
+            // 
+            this.textBoxPoCo.Location = new System.Drawing.Point(223, 172);
+            this.textBoxPoCo.Name = "textBoxPoCo";
+            this.textBoxPoCo.Size = new System.Drawing.Size(97, 26);
+            this.textBoxPoCo.TabIndex = 8;
+            // 
+            // textBoxPhone
+            // 
+            this.textBoxPhone.Location = new System.Drawing.Point(558, 12);
+            this.textBoxPhone.Name = "textBoxPhone";
+            this.textBoxPhone.Size = new System.Drawing.Size(135, 26);
+            this.textBoxPhone.TabIndex = 10;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(558, 45);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(160, 26);
+            this.textBoxEmail.TabIndex = 12;
+            // 
+            // textBoxJobID
+            // 
+            this.textBoxJobID.Location = new System.Drawing.Point(558, 87);
+            this.textBoxJobID.Name = "textBoxJobID";
+            this.textBoxJobID.Size = new System.Drawing.Size(35, 26);
+            this.textBoxJobID.TabIndex = 14;
+            // 
+            // textBoxMCost
+            // 
+            this.textBoxMCost.Location = new System.Drawing.Point(558, 130);
+            this.textBoxMCost.Name = "textBoxMCost";
+            this.textBoxMCost.Size = new System.Drawing.Size(60, 26);
+            this.textBoxMCost.TabIndex = 16;
+            // 
+            // textBoxJCost
+            // 
+            this.textBoxJCost.Location = new System.Drawing.Point(558, 174);
+            this.textBoxJCost.Name = "textBoxJCost";
+            this.textBoxJCost.Size = new System.Drawing.Size(60, 26);
+            this.textBoxJCost.TabIndex = 18;
+            // 
+            // textBoxNotes
+            // 
+            this.textBoxNotes.Location = new System.Drawing.Point(233, 256);
+            this.textBoxNotes.Name = "textBoxNotes";
+            this.textBoxNotes.Size = new System.Drawing.Size(326, 26);
+            this.textBoxNotes.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(57, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 25);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Customer ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 25);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Business Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(174, 25);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Address 1st Line";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(15, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(181, 25);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Address 2nd Line";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(87, 171);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 25);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "Postcode";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(353, 228);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 25);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Notes";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(469, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 25);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "Phone";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(478, 44);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 25);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "Email";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(456, 86);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(87, 25);
+            this.label9.TabIndex = 29;
+            this.label9.Text = "Job No.";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(404, 129);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(139, 25);
+            this.label10.TabIndex = 30;
+            this.label10.Text = "Monthly Cost";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(444, 173);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(99, 25);
+            this.label11.TabIndex = 31;
+            this.label11.Text = "Job Cost";
+            // 
+            // button1st
+            // 
+            this.button1st.Location = new System.Drawing.Point(27, 291);
+            this.button1st.Name = "button1st";
+            this.button1st.Size = new System.Drawing.Size(103, 67);
+            this.button1st.TabIndex = 32;
+            this.button1st.Text = "First Entry";
+            this.button1st.UseVisualStyleBackColor = true;
+            this.button1st.Click += new System.EventHandler(this.button1st_Click);
+            // 
+            // buttonPrev
+            // 
+            this.buttonPrev.Location = new System.Drawing.Point(27, 371);
+            this.buttonPrev.Name = "buttonPrev";
+            this.buttonPrev.Size = new System.Drawing.Size(103, 67);
+            this.buttonPrev.TabIndex = 33;
+            this.buttonPrev.Text = "Previous Entry";
+            this.buttonPrev.UseVisualStyleBackColor = true;
+            this.buttonPrev.Click += new System.EventHandler(this.buttonPrev_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(165, 291);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(103, 67);
+            this.buttonNext.TabIndex = 34;
+            this.buttonNext.Text = "Next Entry";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // buttonLast
+            // 
+            this.buttonLast.Location = new System.Drawing.Point(165, 371);
+            this.buttonLast.Name = "buttonLast";
+            this.buttonLast.Size = new System.Drawing.Size(103, 67);
+            this.buttonLast.TabIndex = 35;
+            this.buttonLast.Text = "Last Entry";
+            this.buttonLast.UseVisualStyleBackColor = true;
+            this.buttonLast.Click += new System.EventHandler(this.buttonLast_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(318, 291);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(103, 67);
+            this.buttonAdd.TabIndex = 36;
+            this.buttonAdd.Text = "Add New Entry";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(318, 371);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(103, 67);
+            this.buttonSave.TabIndex = 37;
+            this.buttonSave.Text = "Save New Entry";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // ConCustomers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonLast);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.buttonPrev);
+            this.Controls.Add(this.button1st);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxNotes);
+            this.Controls.Add(this.textBoxJCost);
+            this.Controls.Add(this.textBoxMCost);
+            this.Controls.Add(this.textBoxJobID);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxPhone);
+            this.Controls.Add(this.textBoxPoCo);
+            this.Controls.Add(this.textBoxAdd2);
+            this.Controls.Add(this.textBoxAdd1);
+            this.Controls.Add(this.textBoxBizName);
+            this.Controls.Add(this.textBoxID);
+            this.Name = "ConCustomers";
+            this.Text = "Contract Customers";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.TextBox textBoxBizName;
+        private System.Windows.Forms.TextBox textBoxAdd1;
+        private System.Windows.Forms.TextBox textBoxAdd2;
+        private System.Windows.Forms.TextBox textBoxPoCo;
+        private System.Windows.Forms.TextBox textBoxPhone;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxJobID;
+        private System.Windows.Forms.TextBox textBoxMCost;
+        private System.Windows.Forms.TextBox textBoxJCost;
+        private System.Windows.Forms.TextBox textBoxNotes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button1st;
+        private System.Windows.Forms.Button buttonPrev;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonLast;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
+
---
+++ ConCustomersForm.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace NickSoftEngA2
+{
+    public partial class ConCustomers : Form
+    {
+        System.Data.SqlClient.SqlConnection newCon;
+        DataSet dsClients;
+        System.Data.SqlClient.SqlDataAdapter daClient;
+        public int whichRec = 0;
+        public int countRecs = 0;
+
+        public ConCustomers()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+            String connectString = ConfigurationManager.ConnectionStrings["BayWynConnectionString"].ConnectionString;
+            newCon = new System.Data.SqlClient.SqlConnection(connectString);
+            
+            dsClients = new DataSet();
+
+            newCon.Close();
+
+            dsClients = new DataSet();
+
+            String sqlGetClient;
+            sqlGetClient = "SELECT * From ContractCustomers";
+
+            daClient = new System.Data.SqlClient.SqlDataAdapter(sqlGetClient, newCon);
+
+            daClient.Fill(dsClients, "Clients");
+            countRecs = dsClients.Tables["Clients"].Rows.Count;
+
+            MoveRecords();
+
+        }
+
+        private void MoveRecords() // populates form with data from cabin table
+        {
+            DataRow clients = dsClients.Tables["Clients"].Rows[whichRec];
+             
+            textBoxID.Text = clients[0].ToString();
+            textBoxBizName.Text = clients[1].ToString();
+            textBoxAdd1.Text = clients[2].ToString();
+            textBoxAdd2.Text = clients[3].ToString();
+            textBoxPoCo.Text = clients[4].ToString();
+            textBoxPhone.Text = clients[5].ToString();
+            textBoxEmail.Text = clients[6].ToString();
+            textBoxJobID.Text = clients[7].ToString();
+            textBoxMCost.Text = clients[8].ToString();
+            textBoxJCost.Text = clients[9].ToString();
+            textBoxNotes.Text = clients[10].ToString();
+
+
+
+        }
+
+        private void button1st_Click(object sender, EventArgs e)
+        {
+            whichRec = 0;
+            MoveRecords();
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            if (whichRec < countRecs - 1)
+            {
+                whichRec++;
+                MoveRecords();
+            }
+            else
+            {
+                MessageBox.Show("You have reached the last entry.");
+            }
+        }
+
+        private void buttonPrev_Click(object sender, EventArgs e)
+        {
+            if (whichRec > 0)
+            {
+                whichRec--;
+                MoveRecords();
+            }
+            else
+            {
+                MessageBox.Show("You are at the first entry.");
+            }
+        }
+
+        private void buttonLast_Click(object sender, EventArgs e)
+        {
+            whichRec = countRecs - 1;
+            MoveRecords();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            textBoxID.Clear();     //clears text boxes
+            textBoxBizName.Clear();
+            textBoxAdd1.Clear();
+            textBoxAdd2.Clear();
+            textBoxPoCo.Clear();
+            textBoxPhone.Clear();
+            textBoxEmail.Clear();
+            textBoxJobID.Clear();
+            textBoxMCost.Clear();
+            textBoxJCost.Clear();
+            textBoxNotes.Clear();
+
+            textBoxID.Hide(); // hides ID as auto-increment
+            button1st.Enabled = false; // stops button functions
+            buttonNext.Enabled = false;
+            buttonPrev.Enabled = false;
+            buttonLast.Enabled = false;
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            DataRow clients = dsClients.Tables["Clients"].NewRow();
+
+            clients[1] = textBoxBizName.Text;
+            clients[2] = textBoxAdd1.Text;
+            clients[3] = textBoxAdd2.Text;
+            clients[4] = textBoxPoCo.Text;
+            clients[5] = textBoxPhone.Text;
+            clients[6] = textBoxEmail.Text;
+            //clients[7] = textBoxJobID.Text;
+            clients[8] = textBoxMCost.Text;
+            clients[9] = textBoxJCost.Text;
+            clients[10] = textBoxNotes.Text;
+
+            dsClients.Tables["Clients"].Rows.Add(clients);
+
+            countRecs++;
+            whichRec = countRecs - 1;
+
+            textBoxID.Show(); // hides ID as auto-increment
+            button1st.Enabled = true; // stops button functions
+            buttonNext.Enabled = true;
+            buttonPrev.Enabled = true;
+            buttonLast.Enabled = true;
+
+            System.Data.SqlClient.SqlCommandBuilder myUpdateDB; // updates database
+            myUpdateDB = new System.Data.SqlClient.SqlCommandBuilder(daClient);
+            myUpdateDB.DataAdapter.Update(dsClients.Tables["Clients"]);
+
+            MoveRecords();
+
+        }
+    }
+}
